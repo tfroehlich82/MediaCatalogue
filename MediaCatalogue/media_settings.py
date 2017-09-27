23d472a85bb209341962de318857a206e204e28d
Thorsten Fröhlich <thorsten.froehlich@metromec.ch>
2017-09-27 16:52:31 +0200

    Serving images

---
+++ MediaCatalogue/media_settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module Documentation
+"""
+
+
+from django.conf import settings
+
+
+PATH = settings.MEDIA_ROOT
+IMAGE_EXT = ['.png', '.jpg', '.jpeg', '.gif']
--- MediaCatalogue/urls.py
+++ MediaCatalogue/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import IndexPage
 
 urlpatterns = [
     url(r'^$', IndexPage.as_view(), name='index'),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
